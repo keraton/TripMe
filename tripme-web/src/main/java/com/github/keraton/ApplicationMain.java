7bef9cbe47dc68ba1c9dc96a3f2008190215f20e
Bowie <keratonjava@gmail.com>
2018-05-14 08:49:04 +0200

    Add Logback

--- tripme-web/src/main/java/com/github/keraton/ApplicationMain.java
+++ tripme-web/src/main/java/com/github/keraton/ApplicationMain.java
@@ -1,8 +1,12 @@
 package com.github.keraton;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import com.github.keraton.configuration.TripmeConfig;
 import com.github.keraton.handler.TripSearchHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,12 +15,18 @@ import java.util.concurrent.Executors;
 
 public class ApplicationMain {
 
+
+    private static Logger logger = LoggerFactory.getLogger(ApplicationMain.class);
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Start Server");
+        logger.info("Start Server");
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(loggerContext);
 
         ApplicationContext ctxJavaConfig = new AnnotationConfigApplicationContext(TripmeConfig.class);
         TripSearchHandler tripSearchHandler = ctxJavaConfig.getBean(TripSearchHandler.class);
-
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/trip", tripSearchHandler);
